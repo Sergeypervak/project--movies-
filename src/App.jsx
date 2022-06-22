4d868ec2c8e0d84afd6bd55c4b3af97a2ad85877
Sergeypervak <har4enko1983@gmail.com>
2022-06-22 23:16:43 +0300

    +some content

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import {Header} from "./layout/Header"
 import {Footer} from "./layout/Footer"
-
+import {Main} from "./layout/Main"
 function App() {
 
   return (
    <>
    <Header/>
    <Footer/>
+   <Main/>
    </> 
      
     
---
+++ src/layout/Main.jsx
@@ -0,0 +1,11 @@
+function Main() {
+    return (<main className="container content">
+        Самые лучшие фильмы - ЗДЕСЬ !
+        
+    </main>
+    )
+    
+
+}
+
+export {Main}
