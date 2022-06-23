083303de9296a2e78e89f0d858d79c33e287ec82
Sergeypervak <har4enko1983@gmail.com>
2022-06-23 23:27:38 +0300

    API OMDB

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,10 +1,9 @@
 function Footer() {
 return (
 <footer className="page-footer #546e7a blue-grey darken-1">
-
 <div className="footer-copyright">
   <div className="container">
-  © {new Date().getFullYear()} Copyright Text
+  © {new Date().getFullYear()} All rights reserved by BUBUK
   <a className="grey-text text-lighten-4 right" href="#!">Repo</a>
   </div>
 </div>
