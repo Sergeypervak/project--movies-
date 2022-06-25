70b3bf65cfd0fc4fbf78232f390b4231437561ba
Sergeypervak <har4enko1983@gmail.com>
2022-06-25 23:37:33 +0300

    slow move

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,11 +1,34 @@
-function Main() {
-    return (<main className="container content">
-        Самые лучшие фильмы - ЗДЕСЬ !
-        
-    </main>
-    )
-    
+import React from "react";
+import {Movies} from "../components/Movies"
 
+class Main extends React.Component {
+    state ={
+        movies: [],
+    }
+    
+    componentDidMount() {
+        fetch('http://www.omdbapi.com/?apikey=55d9d088&s=batman')
+       .then(response => response.json()) 
+       .then(data => this.setState({movies: data.Search}))
 }
+    
+    render () {
+        const {movies} = this.state;
+      
+      return <main className="container content">
+          {
+           movies.length ? (
+           <Movies movies={this.state.movies}/>
+           ) : <h5>Loading...</h5>
+           
+        }
+               </main>
+           
+        
+    }
+}
+
+
+    
 
 export {Main}
