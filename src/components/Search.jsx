88b76c6182965ae68f8fdae03c79d00f1161ed2a
Sergeypervak <har4enko1983@gmail.com>
2022-06-28 16:35:20 +0300

    handleFilter search + type

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,66 +1,90 @@
 import React from "react";
 
 class Search extends React.Component {
-state = {
-   search: '', 
-   type: 'all',
+    state = {
+        search: "",
+        type: "all",
+    };
 
+    handleKey = (event) => {
+        if (event.key === "Enter") {
+            this.props.searchMovies(this.state.search);
+        }
+    };
 
+    handleFilter = (event) => {
+        this.setState(() => ({type: event.target.dataset.type }), () =>{
+          this.props.searchMovies(this.state.search);
+  });
+      };
+
+    render() {
+        return (
+            <div className="row">
+                <div className="col s12">
+                    <div className="input-field ">
+                        <input
+                            placeholder="Название фильма..."
+                            className="validate"
+                            type="search"
+                            value={this.state.search}
+                            onChange={(e) =>
+                                this.setState({ search: e.target.value })
+                            }
+                            onKeyDown={this.handleKey}
+                        />
+
+                        <button
+                            className="btn search-btn"
+                            onClick={() =>
+                                this.props.searchMovies(this.state.search)
+                            }
+                        >
+                            ПОИСК
+                        </button>
+                    </div>
+                    <div>
+                        <label>
+                            <input
+                                className="with-gap"
+                                name="type"
+                                type="radio"
+                                data-type="all"
+                                onChange={this.handleFilter}
+                                checked={this.state.type === 'all'}
+                            />
+                            <span>Films and Series</span>
+                        </label>
+
+                        <label>
+                            <input
+                                className="with-gap"
+                                name="type"
+                                type="radio"
+                                data-type="movie"
+                                onChange={this.handleFilter}
+                                checked={this.state.type === 'movie'}
+                           />
+                            <span>only Films</span>
+                        </label>
+
+                        <label>
+                            <input
+                                className="with-gap"
+                                name="type"
+                                type="radio"
+                                data-type="series"
+                                onChange={this.handleFilter}
+                                checked={this.state.type === 'series'}
+                            />
+                            <span>only Series</span>
+                        </label>
+                    </div>
+                </div>
+                
+            </div>
+        );
+    }
 }
 
-handleKey = (event) => {
- if (event.key === 'Enter') {
-    this.props.searchMovies (this.state.search);
- }}
-
-handleFilter = (event) => {
-
-}
-
-render () {
-    return   <div className="row">
-    <div className="col s12">
-      <div className="input-field ">
-        <input 
-            placeholder="Название фильма..." 
-            className="validate"
-            type="search" 
-            value={this.state.search}
-            onChange={(e) => this.setState({search: e.target.value})}
-            onKeyDown={this.handleKey}        
-         />
-
-       <button className='btn search-btn' onClick={()=>this.props.searchMovies (this.state.search)}>ПОИСК</button>
-      </div>
-    <div>
-    
-      <label>
-      <input class="with-gap" name="type" type="radio" data-type="" />
-        <span>Films and Series</span>
-      </label>
-   
-    
-      <label>
-      <input class="with-gap" name="type" type="radio" data-type="movie" />
-        <span>only Films</span>
-      </label>
-    
-    
-      <label>
-        <input class="with-gap" name="type" type="radio"data-type="series"  />
-        <span>only Series</span>
-      </label>
-     
-
-    </div>
-    </div>
-      <bt/>
-  </div>
-    
-}
-
-}
-
-
-
-export {Search}
+export { Search };
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -13,8 +13,8 @@ class Main extends React.Component {
        .then(data => this.setState({movies: data.Search}))
 }
     
-    searchMovies = (str) => {
-        fetch(`http://www.omdbapi.com/?apikey=55d9d088&s=${str}` )
+    searchMovies = (str, type = 'all') => {
+        fetch(`http://www.omdbapi.com/?apikey=55d9d088&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
         .then(response => response.json()) 
         .then(data => this.setState({movies: data.Search}))
     }
