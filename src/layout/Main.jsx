e0a9ded1365cc8ca5572575ec427be86ae85ae88
Sergeypervak <har4enko1983@gmail.com>
2022-06-29 15:52:01 +0300

    non pics shablon

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -5,18 +5,19 @@ import {Search} from "../components/Search"
 class Main extends React.Component {
     state ={
         movies: [],
+        loading: true,
     }
     
     componentDidMount() {
         fetch('http://www.omdbapi.com/?apikey=55d9d088&s=matrix' )
        .then(response => response.json()) 
-       .then(data => this.setState({movies: data.Search}))
+       .then(data => this.setState({movies: data.Search, loading: false}))
 }
     
     searchMovies = (str, type = 'all') => {
         fetch(`http://www.omdbapi.com/?apikey=55d9d088&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
         .then(response => response.json()) 
-        .then(data => this.setState({movies: data.Search}))
+        .then(data => this.setState({movies: data.Search, loading: false}))
     }
     
 
